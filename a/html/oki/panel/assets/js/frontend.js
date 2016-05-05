898a584df8a936aa73909fe2187087c22efa3903
Sam Smith <sam@mintcanary.com>
2016-05-05 09:32:57 +0100

    include OK Panel (#5)

---
+++ a/html/oki/panel/assets/js/frontend.js
@@ -0,0 +1,10 @@
+jQuery( document ).ready( function ( e ) {
+  if ( $(document).width() > 560) { 
+    $( "a.ok-ribbon" ).addClass( "closed" ).removeAttr('href');
+    
+    $('a.ok-ribbon').click(function(e) {
+        $(this).toggleClass("closed open");
+        $('#ok-panel').toggleClass("closed open");
+    });
+  }
+});
