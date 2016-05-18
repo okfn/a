4c408a1e9c311766ced217611540be0cb5d77b13
Sam Smith <sam@mintcanary.com>
2016-05-18 14:43:06 +0100

    whitespace

--- a/html/oki/panel/assets/js/frontend.js
+++ a/html/oki/panel/assets/js/frontend.js
@@ -1,7 +1,7 @@
 jQuery( document ).ready( function ( e ) {
-  if ( $(document).width() > 560) { 
+  if ( $(document).width() > 560) {
     $( "a.ok-ribbon" ).addClass( "closed" ).removeAttr('href');
-    
+
     $('a.ok-ribbon').click(function(e) {
         $(this).toggleClass("closed open");
         $('#ok-panel').toggleClass("closed open");
