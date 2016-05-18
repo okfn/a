c366f3aa30a5ae74a2d3ffbe0d73996a76e19dbd
Sam Smith <sam@mintcanary.com>
2016-05-18 18:13:11 +0100

    add breakpoint options (#13)

--- a/html/oki/panel/assets/js/frontend.js
+++ a/html/oki/panel/assets/js/frontend.js
@@ -1,12 +1,22 @@
 jQuery( document ).ready( function ( e ) {
-  if ( $(document).width() > 560) {
+
+  var definedbreakpoint = $("[class*=ok-]").data("breakpoint");
+
+  if(definedbreakpoint) {
+  	var breakpoint = definedbreakpoint.replace("px", "");
+  }
+  else {
+  	var breakpoint = "560";
+  }
+
+  if ( $(document).width() > breakpoint) {
     $( "a.ok-ribbon" ).addClass( "closed" ).removeAttr('href');
 
     $('a.ok-ribbon').click(function(e) {
         $(this).toggleClass("closed open");
         $('#ok-panel').toggleClass("closed open");
     });
-
-    $( "body" ).addClass( "has-ok-panel" );
   }
+
+  $( "body" ).addClass( "has-ok-panel" );
 });
