8983e353a70ac8088d7663eec81bc824fc56a6b8
Sam Smith <sam@mintcanary.com>
2016-05-18 14:55:35 +0100

    Ok panel body class (#12)
    * whitespace
    
    * add body class

--- a/html/oki/panel/assets/js/frontend.js
+++ a/html/oki/panel/assets/js/frontend.js
@@ -1,10 +1,12 @@
 jQuery( document ).ready( function ( e ) {
-  if ( $(document).width() > 560) { 
+  if ( $(document).width() > 560) {
     $( "a.ok-ribbon" ).addClass( "closed" ).removeAttr('href');
-    
+
     $('a.ok-ribbon').click(function(e) {
         $(this).toggleClass("closed open");
         $('#ok-panel').toggleClass("closed open");
     });
+
+    $( "body" ).addClass( "has-ok-panel" );
   }
 });
