0bb34197c1238d01528bb4b557ec91ce731b0129
Paul Walsh <paulywalsh@gmail.com>
2016-05-20 10:39:36 +0300

    Remove jQuery dependency for OKI Panel.

--- a/html/oki/panel/assets/js/frontend.js
+++ a/html/oki/panel/assets/js/frontend.js
@@ -1,22 +1,26 @@
-jQuery( document ).ready( function ( e ) {
+var documentWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+var documentHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+var container = document.getElementById('ok-panel-wrapper');
+var panel = document.getElementById('ok-panel');
+var action = document.querySelector('a.ok-ribbon');
+var breakData = container.dataset.breakpoint;
+var activeBreakpoint = breakData ? breakData.replace('px', '') : '560';
 
-  var definedbreakpoint = $("[class*=ok-]").data("breakpoint");
-
-  if(definedbreakpoint) {
-  	var breakpoint = definedbreakpoint.replace("px", "");
-  }
-  else {
-  	var breakpoint = "560";
-  }
-
-  if ( $(document).width() > breakpoint) {
-    $( "a.ok-ribbon" ).addClass( "closed" ).removeAttr('href');
-
-    $('a.ok-ribbon').click(function(e) {
-        $(this).toggleClass("closed open");
-        $('#ok-panel').toggleClass("closed open");
-    });
-  }
-
-  $( "body" ).addClass( "has-ok-panel" );
-});
+if (documentWidth > activeBreakpoint) {
+  action.classList.add('closed')
+  action.removeAttribute('href');
+  action.addEventListener('click', function(e) {
+    if (action.classList.contains('open')) {
+      action.classList.remove('open');
+      action.classList.add('closed');
+      panel.classList.remove('open');
+      panel.classList.add('closed');
+    } else if (action.classList.contains('closed')) {
+      action.classList.remove('closed');
+      action.classList.add('open');
+      panel.classList.remove('closed');
+      panel.classList.add('open');
+    }
+  });
+  document.body.classList.add('has-ok-panel');
+}
